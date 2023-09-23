9673decee30dc16ac29d054ceec6bb4ada8fa4fb
Josue Sebastiao <josuesalazaku@gmail.com>
2023-09-23 14:34:27 +0200

    pong-game

--- Ball.js
+++ Ball.js
@@ -3,5 +3,24 @@ export default class Ball {
         this.ballElm = ballElm;
     }
 
-    update(delta<)
+    get x() {
+       return parseFloat(getComputedStyle(this.ballElm).getPropertyValue("--x"));
+    }
+
+    set x(value) {
+        this.ballElm.style.setProperty("--x", value)
+    }
+
+    get y() {
+        return parseFloat(getComputedStyle(this.ballElm).getPropertyValue("--y"))
+     }
+
+     set y(value) {
+        this.ballElm.style.setProperty("--y", value)
+    }
+
+
+    update(delta) {
+       
+    }
 }
